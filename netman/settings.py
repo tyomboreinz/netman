d6d85f6cb47c02d0a0a9e8dfb51167525361c360
tyomboreinz <tyo.mboreinz@gmail.com>
2021-08-02 10:45:51 +0700

    db change to sqlite

--- netman/settings.py
+++ netman/settings.py
@@ -80,13 +80,8 @@ WSGI_APPLICATION = 'netman.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'netman',
-        
-        'USER': 'admin',
-        'PASSWORD': 'xxxx',
-        'HOST': '192.168.56.10',
-        'PORT': '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
